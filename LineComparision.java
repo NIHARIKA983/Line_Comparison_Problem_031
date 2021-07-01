227ab139c140b16f042699f1996f2bdcf848a841
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-01 13:24:20 +0530

    Added:To Check The Line is Equal greater or less than other line

---
+++ LineComparision.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparison Program");
+        // Creates an object of Scanner
+        Scanner input = new Scanner(System.in);
+        //Given input value
+        int x1= input.nextInt();
+        int x2= input.nextInt();
+        int y1= input.nextInt();
+        int y2= input.nextInt();
+
+        Integer a1= input.nextInt();
+        Integer a2= input.nextInt();
+        Integer b1= input.nextInt();
+        Integer b2= input.nextInt();
+
+        //Length of line calculation
+        double lengthOne = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        double lengthTwo = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+
+        //Given equal operator
+        if (lengthOne == lengthTwo) {
+            System.out.println("Equal ");
+        } else {
+            System.out.println("Not Equal");
+        }
+
+
+    }
+}
--- LineComparison.java
+++ LineComparison.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Program");
-        System.out.println("Enter the input value");
+        System.out.println("Enter the Integer Value");
         // Creates an object of Scanner
         Scanner input = new Scanner(System.in);
         //Given input value
-        int x1= input.nextInt();
-        int x2= input.nextInt();
-        int y1= input.nextInt();
-        int y2= input.nextInt();
+        Integer x1= input.nextInt();
+        Integer x2= input.nextInt();
+        Integer y1= input.nextInt();
+        Integer y2= input.nextInt();
 
         Integer a1= input.nextInt();
         Integer a2= input.nextInt();
@@ -18,8 +19,8 @@ public class LineComparison {
         Integer b2= input.nextInt();
 
         //Length of line calculation
-        double lengthOne = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        double lengthTwo = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+        Double lengthOne = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        Double lengthTwo = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
 
         //Given equal operator
         if (lengthOne == lengthTwo) {
@@ -28,6 +29,18 @@ public class LineComparison {
             System.out.println("Not Equal");
         }
 
+        //Given compare two value and use compareTo method
+        int diffInLength = lengthOne.compareTo(lengthTwo);
+        if (diffInLength == 0) {
+            System.out.println("lengthOne and lengthTwo are Equal");
+        } else if (diffInLength > 0) {
+            System.out.println("Lengthone is geater than lengthtwo ");
+        } else {
+            System.out.println("Lengthone is less than  lengthTwo ");
+
+        }
+
 
     }
 }
+
